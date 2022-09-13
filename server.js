6895753bd7c64c8b0e6f2b228618c173f67c3c2f
Saifur Rahman Nirob <snirob786@gmail.com>
2022-09-13 18:10:20 +0600

    feat: final update

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const flash = require('express-flash');
 const session = require('express-session');
 const port = process.env.PORT || 3000;
 const initializePassport = require('./passport-config');
-initializePassport(passport, email=>{
-    return users.find(user=> user.email === email)
+initializePassport(passport, email => {
+    return users.find(user => user.email === email)},
+    id => {
+        return users.find(user => user.id === id)
 })
 
-app.set('view engine','ejs');
-app.use(express.urlencoded({ extended: false}))
+const users = [];
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -22,34 +26,38 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session())
 
-app.get('/',(req,res)=>{
-    res.render('index', {title: "Login System"})
+app.get('/', (req, res) => {
+    res.render('index', { title: "Login System" })
 })
 
-app.get('/login',(req,res)=>{
-    res.render('login.ejs', {title: "Login Page"})
+app.get('/login', (req, res) => {
+    res.render('login.ejs', { title: "Login Page" })
 })
 
-app.get('/register',(req,res)=>{
-    res.render('register.ejs', {title: "Register Page"})
+app.get('/register', (req, res) => {
+    res.render('register.ejs', { title: "Register Page" })
 })
 
 // Post API
 
-app.post('/register', async (req,res) =>{
-    try{
+app.post('/register', async (req, res) => {
+    try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        let user = [{
+        users.push({
+            id: Date.now().toString(),
             name: req.body.name,
             email: req.body.email,
             password: hashedPassword
-        }];
-        console.log("Profile: ", user);
-    }catch{
+        });
+        console.log("Profile: ", users);
+        res.redirect('/login')
+    } catch {
         res.redirect('/register')
     }
 })
 
+app.post('/login', passport.authenticate('local', { successRedirect: '/profile', failureRedirect: '/login', failureFlash: true }))
 
 
-app.listen(port, ()=>{console.log("Listening to port: ", port)})
+
+app.listen(port, () => { console.log("Listening to port: ", port) })
